53f6910acb6a84b755232991316979dc0c6ef8c5
Thang Nguyen <thangnguyen24111990@gmail.com>
2021-12-06 18:17:32 +0200

    Change fonts to variable font

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel='preload'
+            href='/fonts/Montserrat-VF.woff2'
+            as='font'
+            crossOrigin='anonymous'
+            type='font/woff2'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
